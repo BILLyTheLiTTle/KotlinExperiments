aad1df1527a992e143d2040395c2ab16c9377e80
BILLyTheLiTTle <littleprog@gmail.com>
2017-06-28 15:33:01 +0300

    Function with expression body

--- src/basics/Functions.kt
+++ src/basics/Functions.kt
@@ -14,8 +14,13 @@ fun main(args: Array<String>){
     val b = 2
     val sumer = sum(a, b)//Oooh, the summer!!!
     println("The sum of $a and $b is $sumer")
+
+    val subt = subtract(a, b)
+    println("The subtraction of $a and $b is $subt")
 }
 
 fun sum(value1: Int, value2: Int): Int{
     return value1 + value2
 }
+
+fun subtract(value1: Int, value2: Int): Int = value1 - value2
