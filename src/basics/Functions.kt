dd3da5e9c9ef5d16fc58ce76fa77d2207ede58fc
BILLyTheLiTTle <littleprog@gmail.com>
2017-06-28 15:28:31 +0300

    Declare a simple function and a couple of variables

--- src/basics/Functions.kt
+++ src/basics/Functions.kt
@@ -7,7 +7,15 @@ package basics
 /*
 Just testing the main!
  */
-fun main(args: Array<String>)
-{
+fun main(args: Array<String>){
     println("Hello, world!")
+
+    val a = 1
+    val b = 2
+    val sumer = sum(a, b)//Oooh, the summer!!!
+    println("The sum of $a and $b is $sumer")
+}
+
+fun sum(value1: Int, value2: Int): Int{
+    return value1 + value2
 }
