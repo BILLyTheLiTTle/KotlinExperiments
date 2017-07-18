ff90e4ac65c667b18f98ebfc09b89d95b361e5b3
Vasilios Tsapalos <littleprog@gmail.com>
2017-07-18 13:56:41 +0300

    Copy function for data classes

--- src/basics/Classes.kt
+++ src/basics/Classes.kt
@@ -134,42 +134,47 @@ data class DataClass(val aString: String, val anInt: Int) {
  */
 data class Equalizer(val lastname: String, val firstName: String)
 
+/* PARADIGM 12
+ Kotlin compiler generates also a "copy" function for data classes.
+ There is no need to create a new class. I will use DataClass instead.
+ */
+
 fun main(args: Array<String>){
-    //PARADIGM 1
+    // PARADIGM 1
     println("PARADIGM 1")
     val person = Person("Bill", 31)
     println("This is ${person.name} and he is ${person.age} years old")
 
-    //PARADIGM 2
+    // PARADIGM 2
     println("\nPARADIGM 2")
     val girl = Woman("Sissy")
     girl.age = 24
     println("My name is ${girl.name} and I am ${girl.age} years old") // It's a woman! She will not tell you her age!
 
-    //PARADIGM 3
+    // PARADIGM 3
     println("\nPARADIGM 3")
     val someClass = SomeClass("SomeClass Item")
     println("${someClass.item}")
     println("${someClass.item1}")
 
-    //PARADIGM 6
+    // PARADIGM 6
     println("\nPARADIGM 6")
     val oneMore = OneMoreClass(item1 = 0)
     println("Item 1: ${oneMore.item1} and Item 2: ${oneMore.item2}")
 
-    //PARADIGM 7
+    // PARADIGM 7
     println("\nPARADIGM 7")
     val aClass = AClass(1)
     println("Value 1: ${aClass.value1}, Value 2: ${aClass.value2} and Value 3: ${aClass.value3}")
 
-    //PARADIGM 8
+    // PARADIGM 8
     println("\nPARADIGM 8")
     val bClass = BClass()
     //bClass.item1 = 1
     bClass.setFirstItem(1)
     println("Item 1: ${bClass.item1}")
 
-    //PARADIGM 9
+    // PARADIGM 9
     println("\nPARADIGM 9")
     println("\ntoString() function difference")
     val noDataClass = NoDataClass("No Data", 1)
@@ -186,7 +191,7 @@ fun main(args: Array<String>){
     println("Hashcode of Data Class: ${dataClass.hashCode()}")
     println("Hashcode of Data Class 1: ${dataClass1.hashCode()}")
 
-    //PARADIGM 10
+    // PARADIGM 10
     println("\nPARADIGM 10")
     val robert = Equalizer("McCall", "Robert")
     val denzel = robert
@@ -198,11 +203,18 @@ fun main(args: Array<String>){
     println("Compare robert and me using ==: ${robert == me}")
     println("Compare robert and me using ===: ${robert === me}")
 
-    //PARADIGM 11
+    // PARADIGM 11
     println("\nPARADIGM 11")
     val dataClassOne = DataClass("One", 1)
     dataClassOne.anotherString = "First"
     val dataClassTwo = DataClass("One", 1)
     dataClassTwo.anotherString = "Second"
     println("dataClassOne and dataClassTwo are equals: ${dataClassOne.equals(dataClassTwo)}")
+
+    // PARADIGM 12
+    println("\nPARADIGM 12")
+    val originalDataClass = DataClass("One", 1)
+    val copiedDataClass = originalDataClass.copy(anInt = 2)
+    println("Original Data Class: ${originalDataClass.toString()}")
+    println("Copied Data Class: ${copiedDataClass.toString()}")
 }
