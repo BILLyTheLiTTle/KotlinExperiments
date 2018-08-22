2543a493703dfd7e26707b3605220cb3025aeffe
Vasilios Tsapalos <littleprog@gmail.com>
2018-08-22 10:59:12 +0300

    [Koin] Module Namespace more examples

--- src/main/kotlin/koin/ModuleNamespace.kt
+++ src/main/kotlin/koin/ModuleNamespace.kt
@@ -1,17 +1,30 @@
 package koin
 
-import koin.components.Component
-import koin.components.ComponentImplA
-import koin.components.ComponentImplB
+import koin.components.*
 import org.koin.dsl.module.module
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.StandAloneContext
 import org.koin.standalone.inject
 
+// --- START ---
 val moduleMN = module ("module.namespace") {
     module ("a") { single { ComponentImplA("MN ComponentImplA") as Component } }
     module ("b") { single { ComponentImplB("MN ComponentImplB", 0) as Component } }
 }
+
+// OR
+// Uncomment this to see that there is no difference
+/*val moduleMN = module ("module") {
+    module ("namespace") {
+        module("a") { single { ComponentImplA("MN ComponentImplA") as Component } }
+        module("b") { single { ComponentImplB("MN ComponentImplB", 0) as Component } }
+    }
+    // Uncomment below to see the error (Component for "module.namespace.a or b" is not visible in module.another.c
+    /*module ("another") {
+        module("c") { single { BabushkaComponent(get()) } }
+    }*/
+}*/
+// --- END ---
 
 class ModuleNamespace: KoinComponent {
 
@@ -24,8 +37,14 @@ class ModuleNamespace: KoinComponent {
     // private val comp: Component by inject(module = "a")
     // --- END ---
 
+    // Uncomment also here to see the visibility error
+    // private val babComp: BabushkaComponent by inject(module = "module.another.c")
+
     fun printInfo(){
         println(comp.name)
+
+        // Uncomment also here to see the visibility error
+        // println(babComp.comp.name)
     }
 }
 
