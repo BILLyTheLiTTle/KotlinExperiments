be711c3242adcbc7f10a93b65b39157f0f62eab7
Vasilios Tsapalos <littleprog@gmail.com>
2018-08-22 12:29:04 +0300

    [Koin] Naming Definitions more examples

--- src/main/kotlin/koin/NamingDefinition.kt
+++ src/main/kotlin/koin/NamingDefinition.kt
@@ -4,6 +4,7 @@ import koin.components.Component
 import koin.components.ComponentImplA
 import koin.components.ComponentImplB
 import org.koin.dsl.module.module
+import org.koin.dsl.path.moduleName
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.StandAloneContext
 import org.koin.standalone.inject
@@ -13,17 +14,25 @@ val moduleND = module {
     single("ComponentImplB") { ComponentImplB("ND ComponentImplB", 0) as Component}
 }
 
+val name = ComponentImplA::class.moduleName
+val moduleND2 = module (name){
+    single { ComponentImplA("ND: $name")}
+}
+
 class NamingDefinition: KoinComponent {
 
     private val comp: Component by inject(name = "ComponentImplB")
 
+    private val comp2: ComponentImplA by inject(module = name)
+
     fun printInfo(){
         println(comp.name)
+        println(comp2.name)
     }
 }
 
 fun main(args: Array<String>) {
-    StandAloneContext.startKoin(listOf(moduleND))
+    StandAloneContext.startKoin(listOf(moduleND, moduleND2))
     NamingDefinition().printInfo()
 
 }
